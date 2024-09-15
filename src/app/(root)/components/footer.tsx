287ec17689c82a25cecf08ec6a95a07108ebb877
Kayro França <kayro.dev@gmail.com>
2024-09-15 17:10:23 -0300

    feat: add footer section home

--- src/app/(root)/components/footer.tsx
+++ src/app/(root)/components/footer.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <div
-      className={`bg-[url('/assets/img/footer.webp')] bg-fixed bg-center h-[100dvh] flex items-center`}
+      className={`bg-[url('/assets/img/footer.webp')] bg-fixed bg-center bg-cover h-[100dvh] flex items-center relative z-1 before:absolute before:top-0 before:left-0 before:w-full before:h-1/3 before:z-[9] before:bg-gradient-to-b before:from-black before:to-transparent before:to-33%`}
     >
-      <div className="container px-24 mx-auto grid justify-start items-start gap-32">
+      <div className="container sm:w-4/5 min-[1920]:w-9/12 px-4 sm:px-0 mx-auto flex flex-wrap  md:justify-between md:items-center gap-32 ">
         <div className="grid justify-start items-start gap-8">
-          <h3 className=" text-5xl sm:text-6xl font-semibold -tracking-[3px]">
+          <h3 className="text-4xl sm:text-6xl font-semibold -tracking-[3px]">
             Fale com <br /> a gente
           </h3>
           <Button asChild size="lg">
@@ -28,7 +28,7 @@ export default function Footer() {
             <Link
               href="https://goo.gl/maps/qj6AaJUwxCz4UtMx7"
               target="_blank"
-              className="uppercase w-fit px-10 tracking-wider"
+              className="uppercase w-fit px-10 tracking-[4px]"
               rel="noreferrer"
             >
               Ver no mapa
