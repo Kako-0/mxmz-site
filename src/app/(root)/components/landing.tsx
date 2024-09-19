1e63243a7d6f5f7a0b8ce9c611f4ee7299fd63d5
Kayro França <kayro.dev@gmail.com>
2024-09-19 17:53:05 -0300

    fix: change tag Link to p

--- src/app/(root)/components/landing.tsx
+++ src/app/(root)/components/landing.tsx
@@ -7,17 +7,17 @@ export default function Landing() {
       className="flex flex-col gap-32 justify-center min-h-dvh landing lg:flex-row lg:gap-2 lg:items-center lg:justify-between container sm:w-4/5 min-[1920]:w-9/12 mx-auto px-4 sm:px-0"
       id="landing"
     >
-      <Link className="landing__link" href="/portfolio">
+      <p className="landing__link pointer-events-none">
         Tecnologia
         <span>
           Identificar necessidades, problemas e oportunidades, de forma a propor soluções
           interativas que facilitem a comunicação e otimizem processos.
         </span>
-      </Link>
-      <Link className="landing__link self-end text-right lg:self-auto" href="/portfolio">
+      </p>
+      <p className="landing__link self-end text-right lg:self-auto pointer-events-none">
         Comunicação
         <span>Nosso objetivo é criar experiências interativas que aproximem pessoas e marcas.</span>
-      </Link>
+      </p>
     </div>
   );
 }
