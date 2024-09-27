84d0e675525f390ded3b11f3326b316d9b0b9819
Kayro França <kayro.dev@gmail.com>
2024-09-27 18:19:49 -0300

    feat: add animation on section landing

--- src/app/(root)/components/landing.tsx
+++ src/app/(root)/components/landing.tsx
@@ -1,5 +1,13 @@
-import Link from 'next/link';
+'use client';
+
 import '@/styles/landing.css';
+import { motion } from 'framer-motion';
+
+const variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { ease: 'easeIn', duration: 0.2, delay: 1.2 },
+};
 
 export default function Landing() {
   return (
@@ -7,17 +15,27 @@ export default function Landing() {
       className="flex flex-col gap-32 justify-center min-h-dvh landing lg:flex-row lg:gap-2 lg:items-center lg:justify-between container sm:w-4/5 min-[1920]:w-9/12 mx-auto px-4 sm:px-0"
       id="landing"
     >
-      <p className="landing__link pointer-events-none">
+      <motion.p
+        className="landing__link pointer-events-none"
+        initial={variants.initial}
+        animate={variants.animate}
+        transition={variants.transition}
+      >
         Tecnologia
         <span>
           Identificar necessidades, problemas e oportunidades, de forma a propor soluções
           interativas que facilitem a comunicação e otimizem processos.
         </span>
-      </p>
-      <p className="landing__link self-end text-right lg:self-auto pointer-events-none">
+      </motion.p>
+      <motion.p
+        className="landing__link self-end text-right lg:self-auto pointer-events-none"
+        initial={variants.initial}
+        animate={variants.animate}
+        transition={variants.transition}
+      >
         Comunicação
         <span>Nosso objetivo é criar experiências interativas que aproximem pessoas e marcas.</span>
-      </p>
+      </motion.p>
     </div>
   );
 }
